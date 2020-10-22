d56b44a226cfd81554b47fac682e9c59e4d96a76
Yen Tran <yen.o.tran@tuni.fi>
2020-10-22 22:21:15 +0300

    fix Profile.js

--- models/Profile.js
+++ models/Profile.js
@@ -1,3 +1,121 @@
-const mongoose = require("mongoose");
+const mongoose= require("mongoose");
 // profile must associate with user
-const ProfileSchema = new mongoose.Schema({});  
+// preference to user
+const ProfileSchema=new mongoose.Schema({
+  user:{
+    type: mongoose.Schema.Types.ObjectId, // connect with _id in User
+    ref="user" // User model
+  },
+  company:{
+      type: String,
+  },
+  websites: {
+      type:String,
+  },
+  location: {
+      type:String,
+  },
+  status: {
+      type:String,
+      required:true,
+  },
+  skills: {
+      type:[String], //array of skills
+      required: true,
+  },
+  hobbies: {
+      type:[String],
+  },
+  bio: {
+      type:String,
+  },
+  githubusername: {
+      type:String,
+  },
+  websites: {
+      type:String,
+  },
+  experience:[
+      {
+          title:{
+              type:String,
+              required:true,
+          },
+          company:{
+              type:String,
+              required:true,
+          },
+          location:{
+              type:String,
+          },
+          from:{
+              type:Date,
+              required:true,
+          },
+          to:{
+              type:Date,
+          },
+          current:{
+              type:Boolean,
+              default:false,
+          },
+          description:{
+              type:String,
+          }
+      }
+  ],
+  education:[
+      {
+          school:{
+              type:String,
+              required:true,
+          },
+          degree:{
+              type:String,
+              required:true,
+          },
+          fieldofstudy:{
+              type:String,
+              required:true,
+          },
+          from:{
+              type:Date,
+              required:true,
+          },
+          to: {
+              type:Date,
+          },
+          current:{
+              type:Boolean,
+              default:false,
+          },
+          description:{
+              type:String,
+          }
+      }
+  ],
+  social:{
+      youtube:{
+          type:String,
+      },
+      twitter:{
+          type:String,
+      },
+      facebook:{
+          type:String,
+      },
+      linkedin:{
+          type:String,
+      },
+      instagram:{
+          type:String,
+      },
+  },
+  date:{
+      type:Date,
+      default:false,
+  }
+
+});
+
+module.exports=Profile=mongoose.model("profile",ProfileSchema);
--- models/profile.js
+++ models/profile.js
@@ -1,121 +1,120 @@
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 // profile must associate with user
 // preference to user
-const ProfileSchema=new mongoose.Schema({
-  user:{
+const ProfileSchema = new mongoose.Schema({
+  user: {
     type: mongoose.Schema.Types.ObjectId, // connect with _id in User
-    ref='user' // User model
+    ref: "user", // User model
   },
-  company:{
-      type: String,
+  company: {
+    type: String,
   },
   websites: {
-      type:String,
+    type: String,
   },
   location: {
-      type:String,
+    type: String,
   },
   status: {
-      type:String,
-      required:true,
+    type: String,
+    required: true,
   },
   skills: {
-      type:[String], //array of skills
-      required: true,
+    type: [String], //array of skills
+    required: true,
   },
   hobbies: {
-      type:[String],
+    type: [String],
   },
   bio: {
-      type:String,
+    type: String,
   },
   githubusername: {
-      type:String,
+    type: String,
   },
   websites: {
-      type:String,
+    type: String,
   },
-  experience:[
-      {
-          title:{
-              type:String,
-              required:true,
-          },
-          company:{
-              type:String,
-              required:true,
-          },
-          location:{
-              type:String,
-          },
-          from:{
-              type:Date,
-              required:true,
-          },
-          to:{
-              type:Date,
-          },
-          current:{
-              type:Boolean,
-              default:false,
-          },
-          description:{
-              type:String,
-          }
-      }
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true,
+      },
+      company: {
+        type: String,
+        required: true,
+      },
+      location: {
+        type: String,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
   ],
-  education:[
-      {
-          school:{
-              type:String,
-              required:true,
-          },
-          degree:{
-              type:String,
-              required:true,
-          },
-          fieldofstudy:{
-              type:String,
-              required:true,
-          },
-          from:{
-              type:Date,
-              required:true,
-          },
-          to: {
-              type:Date,
-          },
-          current:{
-              type:Boolean,
-              default:false,
-          },
-          description:{
-              type:String,
-          }
-      }
+  education: [
+    {
+      school: {
+        type: String,
+        required: true,
+      },
+      degree: {
+        type: String,
+        required: true,
+      },
+      fieldofstudy: {
+        type: String,
+        required: true,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
   ],
-  social:{
-      youtube:{
-          type:String,
-      },
-      twitter:{
-          type:String,
-      },
-      facebook:{
-          type:String,
-      },
-      linkedin:{
-          type:String,
-      },
-      instagram:{
-          type:String,
-      },
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
   },
-  date:{
-      type:Date,
-      default:false,
-  }
-
+  date: {
+    type: Date,
+    default: false,
+  },
 });
 
-module.exports=Profile=mongoose.model("profile",ProfileSchema);
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
